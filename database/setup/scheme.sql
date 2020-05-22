e26e947d89db2fff22b2d57e7850a756e0e83805
Stephan <stephan.schett@sbg.at>
2020-05-22 23:18:42 +0200

    Rename every *_id to id

--- database/setup/scheme.sql
+++ database/setup/scheme.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    user_id       INTEGER PRIMARY KEY,
+    id       INTEGER PRIMARY KEY,
     name          TEXT    NOT NULL,
     password_hash INTEGER NOT NULL,
     creation      INTEGER NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE users
 
 CREATE TABLE messages
 (
-    message_id         INTEGER PRIMARY KEY,
+    id         INTEGER PRIMARY KEY,
     sender             INTEGER NOT NULL,
     receiver           INTEGER NOT NULL,
     text_content       TEXT CHECK (additional_content NOT NULL OR
@@ -22,32 +22,32 @@ CREATE TABLE messages
     date_read          INTEGER,
     last_write         INTEGER NOT NULL,
 
-    FOREIGN KEY (sender) REFERENCES users (user_id),
-    FOREIGN KEY (receiver) REFERENCES users (user_id),
-    FOREIGN KEY (additional_content) REFERENCES blobs (blob_id)
+    FOREIGN KEY (sender) REFERENCES users (id),
+    FOREIGN KEY (receiver) REFERENCES users (id),
+    FOREIGN KEY (additional_content) REFERENCES blobs (id)
 );
 
 CREATE TABLE message_history
 (
-    entry_id        INTEGER PRIMARY KEY,
+    id        INTEGER PRIMARY KEY,
     for_message     INTEGER NOT NULL,
     from_text       TEXT    DEFAULT NULL CHECK (from_text NOT NULL OR
                                                 from_additional NOT NULL),
     from_additional INTEGER DEFAULT NULL,
     creation        INTEGER NOT NULL,
 
-    FOREIGN KEY (for_message) REFERENCES messages (message_id),
-    FOREIGN KEY (from_additional) REFERENCES blobs (blob_id)
+    FOREIGN KEY (for_message) REFERENCES messages (id),
+    FOREIGN KEY (from_additional) REFERENCES blobs (id)
 );
 
 CREATE TABLE blobs
 (
-    blob_id  INTEGER PRIMARY KEY,
+    id  INTEGER PRIMARY KEY,
     type     INTEGER CHECK (type BETWEEN 1 AND 2),
     fs_path  TEXT    NOT NULL,
     author   INTEGER NOT NULL,
     hash     TEXT    NOT NULL,
     creation INTEGER NOT NULL,
 
-    FOREIGN KEY (author) REFERENCES users (user_id)
+    FOREIGN KEY (author) REFERENCES users (id)
 )
