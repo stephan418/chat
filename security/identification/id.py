8f972d0325f70d53430cad6a8de7c6a233b21848
Stephan <stephan.schett@sbg.at>
2020-05-24 18:45:12 +0200

    Shorten the id (Basically a ts now)

--- security/identification/id.py
+++ security/identification/id.py
@@ -1,6 +1,4 @@
 import time
-import random
-from security.encode.number_encode import b64encode
 
 # Initialize some counter and variables
 msg_number = 0
@@ -10,8 +8,8 @@ msg_time = time.time() * 1000
 # This function creates a (unless used too frequently) unique timestamp
 def create_unique_ts_number():
     """
-    Creates a uniq    :return: timestamp
-ue timestamp
+    Creates a unique timestamp
+    :return: timestamp
     """
     global msg_number
     global msg_time
@@ -31,5 +29,4 @@ def create_unique_id() -> int:
     Creates a unique ID to be used in databases
     :return: Integer ID
     """
-    return int(str(int(time.time() * 1000)) + str(create_unique_ts_number()) +
-               str(random.randint(1111111111111, 9999999999999)))
+    return int(str(int(time.time() * 1000)) + str(create_unique_ts_number()))
