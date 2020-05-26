b1835b405b4fa62d0f69ff6707bd4c8ab1bec364
Stephan <stephan.schett@sbg.at>
2020-05-26 22:08:25 +0200

    Add function to edit a message

--- database/actions/message.py
+++ database/actions/message.py
@@ -1,4 +1,4 @@
-from database.objects import message
+from database.objects import message, message_history
 from database.main_db import db
 from security.identification.id import create_unique_id
 import time
@@ -30,5 +30,32 @@ def create_message(sender: int, receiver: int, content: message.Content, date_se
     return msg
 
 
-def delete_message(msg_id: int):
-    db
+def delete_message(msg_id: int, _db=db):
+    """ Remove a single value from the database """
+    if not _db.entry_exists_eq("messages", "id", msg_id):
+        raise ValueError(f"The message with the ID '{msg_id}' does not exist")
+
+    _db.remove_single_item("messages", msg_id)
+
+
+def edit_message(msg_id: int, new_msg: message.Message, _db=db):
+    if not _db.entry_exists_eq("messages", "id", msg_id):
+        raise ValueError(f"The message with the ID '{msg_id}' does not exist")
+
+    if not msg_id == new_msg.id:
+        raise ValueError("msg_id and new_msg.id must be the same")
+
+    old_msg = message.Message.empty()
+
+    _db.read_all_values(old_msg, "messages", msg_id)
+
+    history_id = create_unique_id()
+
+    new_msg.last_write = int(time.time() * 1000)
+
+    history = message_history.MessageHistoryEntry(history_id, old_msg.id, old_msg.text_content,
+                                                  old_msg.additional_content)
+
+    _db.insert_all_values(history, "message_history")
+
+    _db.update_all_values(new_msg, "messages", msg_id)
