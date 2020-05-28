554c3c46244b625faa93d16dfa3bf9b09bfcf20b
Stephan <stephan.schett@sbg.at>
2020-05-28 11:49:38 +0200

    Minor bugfixing

--- database/objects/blob.py
+++ database/objects/blob.py
@@ -33,15 +33,16 @@ class Blob(DBObject):
     """
     def __init__(self, blob_id: int, blob_type: int, author: int, blob_hash: int = None, path: str = None,
                  creation: int = None):
-        if not 0 <= blob_type <= 5:
-            raise ValueError(f"{blob_type} is not a valid type id (0-5)")
+        if blob_type is not None:
+            if not 0 <= blob_type <= 5:
+                raise ValueError(f"{blob_type} is not a valid type id (0-5)")
 
         self.id = blob_id
         self.type = blob_type
         self.fs_path = path
         self.author = author
         self.hash = blob_hash
-        self.creation = creation or time.time() * 1000
+        self.creation = creation or int(time.time() * 1000)
 
     @staticmethod
     def empty():
