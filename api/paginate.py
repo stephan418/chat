e4e084b0e0766659690c54bf7f2125fd6d0042fd
Stephan <stephan.schett@sbg.at>
2020-06-09 18:31:03 +0200

    Add type cast

--- api/paginate.py
+++ api/paginate.py
@@ -1,9 +1,9 @@
-def paginate(req: dict, **kwargs):
+def paginate(args: dict, **kwargs):
     """
     Paginate a request
-    :param req: Request to paginate
+    :param args: Request args to paginate
     :param kwargs: Form: key=value where key is the arg key and value is the default value for a given key
     :return: Tuple with pagination
     """
-    t = (req.get(x, y), for x, y in kwargs.items())
+    t = (type(y)(args.get(x, y)) if y is not None else args.get(x, y) for x, y in kwargs.items())
     return t
