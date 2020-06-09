068863ad5ccf35c2358eee3d9758b697a6272730
Stephan <stephan.schett@sbg.at>
2020-06-09 18:34:02 +0200

    Finish all_user endpoint

--- api/endpoints/user/user.py
+++ api/endpoints/user/user.py
@@ -1,10 +1,12 @@
-
 """ Endpoints for general User actions """
 
 # Implement the user_endpoint with url_prefix = 'user'
 
 from api.paginate import paginate
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from api.HTTPErrors import APIError
+from database.actions.user import get_all_users
+from database.db import MDB
 
 user_endpoint = Blueprint("User endpoint", __name__)
 
@@ -13,16 +15,46 @@ user_endpoint = Blueprint("User endpoint", __name__)
 # and c is one of 'name', 'id', 'creation' and d is True or False
 @user_endpoint.route('/', methods=['GET'])
 def get_user_root():
-    per_page, page, order_by, desc = paginate(request, per_page=10, page=0, order_by='name', descending=False)
+    # Create db connection (Thread) TODO: Investigate
+    db = MDB('test.db')
+
+    per_page, page, order_by, desc = paginate(request.args, per_page=10, page=0, order_by='name', descending="False")
     if per_page > 50:
-        return ValueError('per_page must not be greater than 50')
+        raise APIError('per_page must not be greater than 50', 'INVALID_PAGINATION', 400)
 
     if order_by not in ['name', 'id', 'creation']:
-        raise ValueError('order_by must be either name, id or creation')
+        raise APIError('order_by must be either name, id or creation', 'INVALID_VALUE', 400)
 
     if desc.casefold() == 'true':
         desc = True
     elif desc.casefold() == 'false':
         desc = False
     else:
-        raise ValueError('descending must be either true or false')
+        raise APIError('descending must be either true or false', 'INVALID_VALUE', 400)
+
+    if page < 0 or per_page <= 0:
+        raise APIError('page must be larger than per_page larger or equal to 1', 'INVALID_PAGINATION', 400)
+
+    users = get_all_users(per_page * page, (page + 1) * per_page, order_by, desc, _db=db)
+
+    if len(users) <= 0:
+        raise APIError('page out of range', 'PAGE_OUT_OF_RANGE', payload={
+            "items": db.get_number_of_items_in_table('users')
+        })
+
+    if len(users) <= 0:
+        result = [{}]
+    else:
+        keys = list(users[0].__dict__.keys())
+
+        disallowed = ['password_hash', 'last_write', 'email']
+        for key in disallowed:
+            keys.remove(key)
+
+        result = list()
+        for user in users:
+            result.append({k: user.get_item(k) for k in keys})
+
+    response = jsonify(result)
+
+    return response
