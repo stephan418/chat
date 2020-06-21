421a63b4b646e15e05ad35cf4f8db2ece6d03737
Stephan <stephan.schett@sbg.at>
2020-06-21 11:51:47 +0200

    Add the ability to specify keys to be encoded

--- api/common.py
+++ api/common.py
@@ -1,6 +1,11 @@
 from security.encode import number_encode
 
 
-def encode_if_id(key: str, value):
+def encode_if_id(key: str, value, encode: list = None):
     """ Encode and pad the value if it is under the key 'id' """
-    return value if key != 'id' else number_encode.b64encode_pad(value, 11)
+    if encode is None:
+        encode = ['key']
+
+    print(value, key)
+
+    return value if key not in encode else number_encode.b64encode_pad(value, 11)
